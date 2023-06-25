69c4f23a412e173f682b590b11b3c3b983f9feec
Tomoya Wada <tomoyawada@tomoyas-mbp-2.mynetworksettings.com>
2023-06-24 22:41:40 -0400

    add package managment files

--- src/DefinitionIngestor.py
+++ src/DefinitionIngestor.py
@@ -3,10 +3,9 @@ import re
 
 
 class Utilities:
-
     def __init__(self, file_path_definition, categories):
-        self.definitions = json.load(open(file_path_definition, 'r'))
-        self.category_name_base = 'base'
+        self.definitions = json.load(open(file_path_definition, "r"))
+        self.category_name_base = "base"
         self.regexes = {}
         self.regex_id = 0
         self.dict_regex_ids = {}
@@ -14,42 +13,42 @@ class Utilities:
     def load_regexes_base(self):
         _regexes = {}
         _dict_regex_ids = {}
-        for k,v in self.definitions[self.category_name_base].items():
-            _pattern = f'[{v}]'
+        for k, v in self.definitions[self.category_name_base].items():
+            _pattern = f"[{v}]"
             _regexes[self.regex_id] = {
-                'category': self.category_name_base,
-                'name': k,
-                'tags': [k],
-                'pattern': _pattern,
-                'regex': re.compile(_pattern)
-            }
-            _dict_regex_ids[k] = self.regex_id
-            self.regex_id += 1
-        return (_regexes, _dict_regex_ids)        
-
-    def load_regexes_per_category(self, category: str):
-        """ """
-        _regexes = {}
-        _dict_regex_ids = {}
-        for k,v in self.definitions[category].items():
-            _pattern = '['
-            for regex_type in v:
-                _idx = self.dict_regex_ids[regex_type]
-                _obj = self.regexes[_idx]
-                _pattern += _obj['pattern'][1:-1] # exclude squre brackets
-            _pattern += ']'
-            _regexes[self.regex_id] = {
-                'category': category,
-                'name': k,
-                'tags': [k],
-                'pattern': _pattern,
-                'regex': re.compile(_pattern)
+                "category": self.category_name_base,
+                "name": k,
+                "tags": [k],
+                "pattern": _pattern,
+                "regex": re.compile(_pattern),
             }
             _dict_regex_ids[k] = self.regex_id
             self.regex_id += 1
         return (_regexes, _dict_regex_ids)
 
-    
+    def load_regexes_per_category(self, category: str):
+        """ """
+        _regexes = {}
+        _dict_regex_ids = {}
+        for k, v in self.definitions[category].items():
+            _pattern = "["
+            for regex_type in v:
+                _idx = self.dict_regex_ids[regex_type]
+                _obj = self.regexes[_idx]
+                _pattern += _obj["pattern"][1:-1]  # exclude squre brackets
+            _pattern += "]"
+            _regexes[self.regex_id] = {
+                "category": category,
+                "name": k,
+                "tags": [k],
+                "pattern": _pattern,
+                "regex": re.compile(_pattern),
+            }
+            _dict_regex_ids[k] = self.regex_id
+            self.regex_id += 1
+        return (_regexes, _dict_regex_ids)
+
+
 def ingest_definition(file_path_definition: str, categories: list):
     utils = Utilities(file_path_definition, categories)
     for category in categories:
--- src/RegexGenerator.py
+++ src/RegexGenerator.py
@@ -6,22 +6,22 @@ from src.DefinitionIngestor import ingest_definition
 
 class RegexGenerator:
     def __init__(self, file_path_config):
-        config = json.load(open(file_path_config, 'r'))
+        config = json.load(open(file_path_config, "r"))
         _regexes, _dict_regex_ids = ingest_definition(
-            file_path_definition=config['file_path_definition'],
-            categories=config['categories']
+            file_path_definition=config["file_path_definition"],
+            categories=config["categories"],
         )
         self.regexes = _regexes
         self.dict_regex_ids = _dict_regex_ids
-        self.regex_types_coase = config['regex_types_coase']
+        self.regex_types_coase = config["regex_types_coase"]
         self.regex_selected_coase = self.select_regex_coase()
-        self.regex_unrecognized_ch = '.'
+        self.regex_unrecognized_ch = "."
 
     def select_regex_coase(self):
         res = {}
         for regex_type in self.regex_types_coase:
             _idx = self.dict_regex_ids[regex_type]
-            _regex = self.regexes[_idx]['regex']
+            _regex = self.regexes[_idx]["regex"]
             res[_regex] = _idx
         return res
 
@@ -29,37 +29,41 @@ class RegexGenerator:
         indices = []
         for ch in string:
             found_regex = False
-            for regex,idx in self.regex_selected_coase.items():
+            for regex, idx in self.regex_selected_coase.items():
                 if re.match(regex, ch):
-                    indices.append(str(idx)) # string conversion is needed to concatenate
+                    indices.append(
+                        str(idx)
+                    )  # string conversion is needed to concatenate
                     found_regex = True
                     break
-            if found_regex == False:
-                indices.append('A')
+            if found_regex is False:
+                indices.append("A")
         return indices
-    
+
     def compress_encoded_string(self, string: str):
         """
         ref: https://stackoverflow.com/a/59183023
         """
         return (
-            (x, sum(1 for _ in y)) for x, y in groupby(self.encode_string(string))
+            (x, sum(1 for _ in y))
+            for x, y in groupby(self.encode_string(string))
         )
-   
+
     def generate_regex_coase(self, string: str):
-        _regex = ''
-        for k,v in self.compress_encoded_string(string):
+        _regex = ""
+        for k, v in self.compress_encoded_string(string):
             try:
                 _idx = int(k)
-                _regex += self.regexes[_idx]['pattern']
-            except:
+                _regex += self.regexes[_idx]["pattern"]
+            except Exception:
                 _regex += self.regex_unrecognized_ch
             if v > 1:
                 _regex += f"{{{v}}}"
         return _regex
 
     def generate_regexes_coase(self, values: list):
-        return [self.generate_regex_coase(v)for v in values]
+        return [self.generate_regex_coase(v) for v in values]
+
 
 # def is_ascii(s):
 #     return all(ord(c) < 128 for c in s)
@@ -67,5 +71,3 @@ class RegexGenerator:
 # def isascii(s):
 #     """Check if the characters in string s are in ASCII, U+0-U+7F."""
 #     return len(s) == len(s.encode())
-
-
